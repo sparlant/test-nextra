6279d53e4f0b8f1abb92b944a65e4147564894b4
Sandra Parlant <sparlant@github.com>
2022-09-28 07:04:16 +0000

    test deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.js',
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 })
 module.exports = withNextra()
